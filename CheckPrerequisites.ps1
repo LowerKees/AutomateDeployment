16a0db530d170bc794746db9ef6f620743379a51
martijn.beenker <martijn.beenker@avanade.com>
2018-12-11 07:48:40 +0100

    initial commit

---
+++ CheckPrerequisites.ps1
@@ -0,0 +1,19 @@
+﻿$CommandLocation = Split-Path $MyInvocation.MyCommand.Path -Parent;
+
+## Include scripts
+try
+{
+    . ("$($CommandLocation)\CheckFunctions.ps1");
+}
+catch
+{
+    Write-Output "Including scripts failed. Exception will be reraised";
+    Throw;
+}
+
+## Checking session for package provider
+InstallPackageProviderIfPossible -PackageProvider "Nuget";
+
+## Checking session for modules
+ImportModule -ModuleName "AzureRM";
+
---
+++ DeployDwh.ps1
@@ -0,0 +1,16 @@
+﻿$DeployResourceGroup = ("$($PSScriptRoot)\DeployResourceGroup.ps1");
+
+
+
+
+
+[string[]]$ResourcesToDeploy = @("DwhServer","Dwh");
+
+foreach($Resource in $ResourcesToDeploy)
+{
+    $TemplateFilePath = ("$($PSScriptRoot)\Templates\$($Resource).json");
+    $ParametersFilePath = ("$($PSScriptRoot)\Templates\$($Resource)Parameters.json");
+
+    $TemplateFilePath;
+    $ParametersFilePath;
+}
